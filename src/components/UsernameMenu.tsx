f8da4b6d25ff116d6ace2755deca941a58c80a05
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2024-03-06 14:35:22 +0100

    logout error fix

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -40,7 +40,13 @@ const UsernameMenu = () => {
         <DropdownMenuItem>
           <Button
             className='flex flex-1 font-bold bg-orange-500'
-            onClick={() => logout()}
+            onClick={() =>
+              logout({
+                logoutParams: {
+                  returnTo: window.location.origin,
+                },
+              })
+            }
           >
             Log Out
           </Button>
